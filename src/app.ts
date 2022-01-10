bf0302e74f7f7ee5cf0b2e02425f5791e6b4e3b4
Gábor Vass <90134776+Vassgabi89@users.noreply.github.com>
2022-01-10 17:08:55 +0100

    done

--- src/app.ts
+++ src/app.ts
@@ -1,5 +1,6 @@
 // Importáld be a HumanHero és TransformerHero osztályokat.
-
+import { HumanHero } from './hero'
+import { TransformerHero } from './transformer'
 
 /**
  * Állítsd be helyesen a változót!
@@ -8,7 +9,31 @@
  * Hozz létre benne 3 HumanHero példányt, szabadon választott adatokkal.
  * @var {HumanHero[]} humans
  */
-export const humans: any[] = [];
+export const humans: HumanHero[] = [
+    {
+        id: 1,
+        name: 'Hulk',
+        photo: 'smaaash.jpg',
+        sex: 'male',
+        age: 35,
+        health: 15000
+    },
+    {
+        id: 2,
+        name: 'Iron Man',
+        sex: 'male',
+        age: 33,
+        health: 6500
+    },
+    {
+        id: 3,
+        name: 'Thor',
+        photo: 'thunder.jpg',
+        sex: 'male',
+        age: 34,
+        health: 14000
+    }
+]
 
 /**
  * Állítsd be helyesen a változót!
@@ -17,4 +42,27 @@ export const humans: any[] = [];
  * Hozz létre benne 3 TransformerHero példányt, szabadon választott adatokkal.
  * @var {TransformerHero[]} transformers
  */
-export const transformers: any[] = [];
+export const transformers: TransformerHero[] = [
+    {
+        id: 1,
+        name: 'Optimus Prime',
+        wings: 0,
+        wheels: 12,
+        clan: 'Autobots'
+    },
+    {
+        id: 2,
+        name: 'Bumblebee',
+        photo: 'bumblebee_fight2.jpg',
+        wings: 2,
+        wheels: 4,
+        clan: 'Autobots'
+    },
+    {
+        id: 3,
+        name: 'Megatron',
+        wings: 2,
+        wheels: 8,
+        clan: 'Decepticons'
+    },
+];
